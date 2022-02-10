5f2eb73f996e6994872c2dbd6185701bdeadb581
funnyboy-roks <funnyboyroks@gmail.com>
2022-02-09 19:10:20 -0600

    :sparkles: Initial Commit

---
+++ hashset/chaining/HashSet.js
@@ -0,0 +1,173 @@
+/**
+ * Implementation of a hashset, used for visualisation
+ */
+class HashSet {
+
+	constructor() {
+		this.size = 0;
+		this.buckets = new Array(13);
+	}
+
+	/**
+	 * Adds an element to the hashset
+	 * @param {Number} element
+	 * @returns {Boolean}
+	 */
+	add(element) {
+		let hash = HashSet.hash(element, this.buckets.length);
+		if (!this.buckets[hash]) {
+			this.buckets[hash] = new HashNode(element);
+			this.size++;
+			return true;
+		}
+		let current = this.buckets[hash];
+		while (current) {
+			if (current.element === element) {
+				return false;
+			}
+			current = current.next;
+		}
+		this.buckets[hash] = new HashNode(element, this.buckets[hash]);
+		this.size++;
+		return true;
+	}
+
+	/**
+	 * Removes an element from the hashset
+	 * @param {Number} element
+	 */
+	remove(element) {
+		let index = HashSet.hash(element, this.buckets.length);
+		if (!this.buckets[index]) {
+			return false;
+		}
+
+		let prev = null;
+		let current = this.buckets[index];
+		while (current) {
+			if (current.element === element) {
+				if (prev) {
+					prev.next = current.next;
+				} else {
+					this.buckets[index] = current.next;
+				}
+				this.size--;
+				return true;
+			}
+			prev = current;
+			current = current.next;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks if an element is in the hashset
+	 * @param {Number} element
+	 * @returns {boolean}
+	 */
+	contains(element) {
+		let index = HashSet.hash(element, this.buckets.length);
+		if (!this.buckets[index]) {
+			return false;
+		}
+		let current = this.buckets[index];
+		while (current) {
+			if (current.element === element) {
+				return true;
+			}
+			current = current.next;
+		}
+		return false;
+	}
+
+	resize(newSize = this.buckets.length * 2) {
+		let newBuckets = new Array(newSize);
+		for (let i = 0; i < this.buckets.length; i++) {
+			let current = this.buckets[i];
+			while (current) {
+				let hash = HashSet.hash(current.element, newBuckets.length);
+				if (!newBuckets[hash]) {
+					newBuckets[hash] = new HashNode(current.element);
+				} else {
+					newBuckets[hash] = new HashNode(current.element, newBuckets[hash]);
+				}
+				current = current.next;
+			}
+		}
+		this.buckets = newBuckets;
+	}
+
+	/**
+	 * Returns the hash of an element
+	 * @param {Number} element
+	 * @param {Number} length
+	 * @returns {number}
+	 */
+	static hash(element, length) {
+		return Math.abs(Math.floor(element)) % length;
+	}
+
+	/**
+	 * Returns a 1d array with the values (used for visualisation)
+	 * @returns {Number[]}
+	 */
+	get flatten() {
+		let result = [];
+		for (let i = 0; i < this.buckets.length; i++) {
+			if (this.buckets[i]) {
+				let current = this.buckets[i];
+				while (current) {
+					result.push(current.element);
+					current = current.next;
+				}
+			}
+		}
+		return result;
+	}
+
+	get nodes() {
+		let result = [];
+		for (let i = 0; i < this.buckets.length; i++) {
+			if (this.buckets[i]) {
+				let current = this.buckets[i];
+				while (current) {
+					result.push(current);
+					current = current.next;
+				}
+			}
+		}
+		return result;
+	}
+
+	get max() {
+		let max = -Infinity;
+		for (let i = 0; i < this.buckets.length; i++) {
+			if (this.buckets[i]) {
+				let current = this.buckets[i];
+				while (current) {
+					if(max < current.element) {
+						max = current.element;
+					}
+					current = current.next;
+				}
+			}
+		}
+		return max === -Infinity ? 50 : max;
+	}
+}
+
+class HashNode {
+
+	/**
+	 *
+	 * @param {Number} element
+	 * @param {HashNode} next
+	 */
+	constructor(element, next = null) {
+		this.element = element;
+		this.next = next;
+		this.color = Math.round(Math.random() * 255);
+	}
+
+
+}
---
+++ hashset/chaining/index.js
@@ -0,0 +1,112 @@
+const set = new HashSet();
+const valuesElt = document.querySelector('#values');
+const actionElt = document.querySelector('#action');
+const inputElt = document.querySelector('#value');
+
+const autoAddRemove = () => {
+	let action = '';
+	if (document.querySelector('#auto-add').checked) {
+		const value = floor(random(rangeMin.valueAsNumber, rangeMax.valueAsNumber));
+		const output = set.add(value);
+		action += `Adding ${ value } - ${ output }`;
+	}
+	if (document.querySelector('#auto-remove').checked) {
+		const value = floor(random(rangeMin.valueAsNumber, rangeMax.valueAsNumber));
+		const output = set.remove(value);
+		action += `\nRemoving ${ value } - ${ output }`;
+	}
+	actionElt.textContent = action.trim();
+};
+
+const addListeners = () => {
+	document.querySelector('#add').addEventListener('click', () => {
+		const value = inputElt.valueAsNumber;
+		const output = set.add(value);
+		actionElt.innerHTML = `Adding ${ value } - ${ output }`;
+	});
+	document.querySelector('#remove').addEventListener('click', () => {
+		const value = inputElt.valueAsNumber;
+		const output = set.remove(value);
+		actionElt.innerHTML = `Removing ${ value } - ${ output }`;
+	});
+};
+
+function setup() {
+	createCanvas(windowWidth * .75, windowHeight * .75);
+
+	setInterval(autoAddRemove, 250);
+	addListeners();
+
+	noStroke();
+	ellipseMode(CENTER);
+	textAlign(CENTER, CENTER);
+}
+
+let rainbowOffset = 0;
+
+function draw() {
+	background(0);
+
+	let cols = set.buckets.length;
+	stroke(255);
+	strokeWeight(1);
+	let colW = width / cols;
+	stroke(150);
+	line(0, colW / 2, width, colW / 2);
+	for (let i = 0; i < cols; i++) {
+		push();
+		fill(255);
+		textSize(colW / 3);
+		text(i, i * colW + colW / 2, colW / 3);
+		pop();
+		line(i * colW, 0, i * colW, height);
+		push();
+		translate(0, colW);
+
+		let node = set.buckets[i];
+		let y = 0;
+		while (node) {
+			let item = node.element;
+			let x = i * colW + colW / 2;
+
+			push();
+			colorMode(HSB);
+			// fill(map(item, 0, set.max, 0, 255), 255, 255, 100);
+			let hue = node.color;
+			if (hue > 255) {
+				hue = 255 - hue % 255;
+			}
+			fill(hue, 255, 255, 100);
+			ellipse(x, y * colW * .85, colW * .7, colW * .7);
+			pop();
+
+			noStroke();
+			fill(0);
+			textSize(colW / 3);
+			text(item, x, y * colW * .85);
+			++y;
+			node = node.next;
+		}
+		pop();
+	}
+	valuesElt.innerHTML = set.nodes.map(n => `<span style="color: hsl(${ n.color }, 100%, 50%);">${ n.element }</span>`).join(', ');
+	rainbowOffset += 1;
+}
+
+const addMany = (count = 10) => {
+	let added = 0;
+	for (let i = 0; i < count; i++) {
+		added += set.add(floor(random(0, 50)));
+	}
+	return added;
+}
+
+const removeMany = (count = 10) => {
+	let removed = 0;
+	for (let i = 0; i < count; i++) {
+		removed += set.remove(floor(random(0, 50)));
+	}
+	return removed;
+}
+
+
